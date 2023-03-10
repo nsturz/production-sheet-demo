2c88c1e3abade4732e48af6a40d4853c6aff82b6
Nicolas Sturz <nicksturz92@gmail.com>
2023-03-10 20:02:16 +0000

    now it begins

---
+++ client/pages/production-sheet.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ProductionSheet extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>SUP!!!!</h1>
+      </div>
+    );
+  }
+}
