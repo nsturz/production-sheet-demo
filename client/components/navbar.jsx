c88f8a054496a9598d57c2c0b693743126c89df9
Nicolas Sturz <nicksturz92@gmail.com>
2023-03-15 18:45:52 +0000

    saving

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -6,7 +6,7 @@ export default class NavBar extends React.Component {
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
           {/* fix this 👇🏼 */}
-          <img src="" alt="" />
+          <img src="../components/images/color-ad-logo.jpg" alt="" />
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
--- client/pages/production-sheet.jsx
+++ client/pages/production-sheet.jsx
@@ -6,22 +6,22 @@ export default class ProductionSheet extends React.Component {
     return (
       <div>
         <NavBar className="navbar" />
-        <div className="row">
-          <h2 className="col">Production Sheet - Week 1</h2>
+        <div className="row mt-4  ms-3 me-3">
+          <h3 className="main-title col">Production Sheet - Week 1</h3>
           <i className=" col-1 fa-solid fa-clipboard-list" />
-          <select className="form-select col  " aria-label="Default select example">
+          <select className="form-select col m-1 " aria-label="Default select example">
             <option selected>Open this select menu</option>
             <option value="1">One</option>
             <option value="2">Two</option>
             <option value="3">Three</option>
           </select>
-          <select className=" col form-select" aria-label="Default select example">
+          <select className=" col form-select m-1" aria-label="Default select example">
             <option selected>Open this select menu</option>
             <option value="1">One</option>
             <option value="2">Two</option>
             <option value="3">Three</option>
           </select>
-          <button type="button" className=" col btn btn-success">Success</button>
+          <button type="button" className=" col-1 btn btn-success m-1">Success</button>
         </div>
       </div>
     );
