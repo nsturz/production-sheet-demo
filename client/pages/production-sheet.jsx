1357a5e28c7368a9e0b8785a75a78d8770807907
Nicolas Sturz <nicksturz92@gmail.com>
2023-03-15 17:14:09 +0000

    began styling production-sheet.jsx

--- client/pages/production-sheet.jsx
+++ client/pages/production-sheet.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import NavBar from '../components/navbar';
 
 export default class ProductionSheet extends React.Component {
   render() {
     return (
-      <div className="container">
-        <h1>SUP!!!!</h1>
-      </div>
+      <NavBar className="navbar"/>
     );
   }
 }
