3684f875c30324657815e4361b9cd6165454c180
Nicolas Sturz <nicksturz92@gmail.com>
2023-03-16 19:32:26 +0000

    continued styling for production sheet page

--- client/pages/production-sheet.jsx
+++ client/pages/production-sheet.jsx
@@ -6,22 +6,41 @@ export default class ProductionSheet extends React.Component {
     return (
       <div>
         <NavBar className="navbar" />
-        <div className="row mt-4  ms-3 me-3">
-          <h3 className="main-title col">Production Sheet - Week 1</h3>
-          <i className=" col-1 fa-solid fa-clipboard-list" />
-          <select className="form-select col m-1 " aria-label="Default select example">
-            <option selected>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
-          <select className=" col form-select m-1" aria-label="Default select example">
-            <option selected>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
-          <button type="button" className=" col-1 btn btn-success m-1">Success</button>
+        <div className="m-3 d-flex ">
+          <div className="col-4">
+            <div className=" d-flex">
+              <h3 className="main-title m-1">
+                Production Sheet |  Week 1
+                <i className="fa-solid fa-clipboard-list ms-3  text-success" />
+              </h3>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="d-flex">
+              <select className="form-select fw-light m-1" aria-label="Default select example">
+                <option selected>Year</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+              <select className=" form-select fw-light m-1" aria-label="Default select example">
+                <option selected>Week</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+          </div>
+          <div className="col">
+            <div className="d-flex m-1">
+              <button type="button" className="btn btn-success ">Search</button>
+            </div>
+          </div>
+        </div>
+        <div className="row m-3">
+          <div className="col-12">
+            <h4 className="fw-light"> Weekly Totals: 424,100</h4>
+          </div>
         </div>
       </div>
     );
