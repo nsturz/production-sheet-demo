0bbfcf82f9c4241672b1637b32cf81caa28ea2a1
Nicolas Sturz <nicksturz92@gmail.com>
2023-03-31 21:15:09 +0000

    working on form layout

--- client/components/new-job-form-modal.jsx
+++ client/components/new-job-form-modal.jsx
@@ -14,26 +14,64 @@ export default function NewJobModal() {
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
             </div>
             <div className="modal-body">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nam corporis odio ut neque repudiandae vel aliquam, provident autem sapiente alias est? Voluptatum unde nihil beatae sed ipsa qui ducimus.
-                Iste facilis nemo dignissimos reiciendis et provident necessitatibus unde aliquam nesciunt, iure voluptatibus quas quaerat amet aliquid error doloribus veniam dolore tempore, quam vitae laborum, voluptatem dolores perferendis. Consequatur, ratione.
-                Atque, ducimus libero! Cum ad accusamus, suscipit rem, blanditiis soluta nihil recusandae ratione necessitatibus numquam maxime quis ullam. Illum nostrum recusandae perferendis. Amet itaque a quis, porro quia aliquid iusto?
-                Nobis a fuga id dolorum, quibusdam, repellat natus corporis perspiciatis enim magnam commodi unde dolores assumenda nostrum accusantium recusandae vel. Et quisquam doloremque impedit! Quidem ut alias quos tenetur obcaecati.
-                Aliquam, cum mollitia rem dicta a libero nostrum, corrupti quidem repudiandae accusamus natus. Deleniti, hic facilis. Repellat ratione aliquid laudantium deserunt mollitia perspiciatis laboriosam, libero hic amet, aliquam id reprehenderit!
-                Repellat quam quaerat, officia iste inventore ut obcaecati molestiae, laudantium debitis cupiditate nihil? Sed cupiditate tenetur reiciendis explicabo quod, libero necessitatibus ad illo dolorem aliquam ratione similique? Reiciendis, nobis praesentium.
-                Provident optio, consectetur minus assumenda ratione facere, dolores, ipsum totam quis ullam officia nulla numquam. Obcaecati natus earum, omnis provident veritatis minima, beatae, odio fuga corrupti non nulla aperiam quae.
-                Sit voluptates tempore tempora dolorem, neque ratione ad iusto voluptas reprehenderit nostrum ipsam ut cupiditate recusandae culpa unde, rem repellendus ab magnam natus qui? Quis tempore magnam eum optio modi?
-                Itaque, explicabo tenetur eum placeat, sed assumenda obcaecati dolore id ut quas suscipit aliquid. Autem iusto cupiditate consequatur sint error blanditiis nemo architecto incidunt distinctio, laborum reprehenderit labore aut eaque.
-                Reiciendis, repellendus! Accusamus pariatur, quae, non error suscipit provident eius aut aspernatur, ea aliquam sit! Magnam quia ducimus saepe? Quos nam iste ipsam eligendi in perspiciatis voluptates deserunt similique assumenda.
-                Ab doloremque officia nisi exercitationem iure magnam rem, accusamus facilis quidem fugit. Eos, nostrum perspiciatis quos unde vero eum consequuntur magnam voluptatum eaque earum voluptatibus accusamus sapiente eveniet exercitationem omnis.
-                Nulla labore officiis autem sint rem. Facere odit doloremque rerum molestiae voluptates quia magnam reiciendis deserunt debitis cupiditate eveniet dignissimos delectus dolore, ut consectetur eligendi quaerat quae natus nesciunt. Cumque!
-                Unde nesciunt voluptatem suscipit dolorum adipisci iste corrupti doloribus iusto natus enim autem rem voluptatum, minima qui quod consequuntur porro fugit perferendis rerum molestias perspiciatis iure ut. Vero, nulla delectus?
-                Debitis impedit cupiditate sed in id quo voluptas amet autem beatae ab tenetur, fugit adipisci eos eius repudiandae commodi? Expedita quod tenetur, ipsa tempore earum veritatis quae sequi cumque libero.
-                Beatae amet quibusdam nam voluptatem voluptatibus nostrum officiis distinctio soluta. Impedit reiciendis a, minus ratione repellat perferendis iste odio. Placeat dolor praesentium quasi necessitatibus cum reiciendis ut porro exercitationem aspernatur!
-                Numquam et, dicta amet error eius libero ex nesciunt! Unde iure debitis incidunt nam, dolores explicabo. Amet, exercitationem mollitia laborum et, consequuntur, similique at quos sed ut debitis nam a.
-                Iusto porro illo vitae qui veniam temporibus quaerat, reprehenderit quae placeat voluptas nulla nihil necessitatibus nam explicabo a. Culpa, officia! Aperiam accusamus dolorum accusantium sequi, quia expedita soluta quas optio.
-                Non nostrum saepe et. Natus facere quos hic quae aperiam dolores vel ex earum totam! Nostrum, adipisci odit animi atque iste voluptas sit, soluta a ad alias quisquam suscipit rerum.
-                Cumque qui non, eius quod earum ex quasi nisi incidunt libero in voluptatem doloremque eum sapiente doloribus nesciunt id, ad repudiandae laudantium fugiat dolore alias quo impedit quia. Fuga, porro.
-                Molestiae id dicta voluptas rerum error ullam. Impedit perferendis perspiciatis nesciunt doloremque. Dignissimos aspernatur reiciendis inventore iusto nostrum nesciunt eaque rerum, quas similique eos! Suscipit accusantium ipsum eos dignissimos dolores?</p>
+              <form action="">
+                <div className="mb-2 mt-2">
+                  <label htmlFor="yearSelect" >Year</label>
+                  <select name="" id="yearSelect" className='form-select fw-light '>
+                    <option value="">2022</option>
+                    <option selected>2023</option>
+                    <option value="">2024</option>
+                  </select>
+                </div>
+                <div className="mb-2 mt-2">
+                  <label htmlFor="weekSelect" >Week</label>
+                  <select name="" id="weekSelect" className='form-select fw-light '>
+                    <option value="1" selected>1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                  </select>
+                </div>
+                <div className="mb-2 mt-2">
+                  <label htmlFor="companyNameInput" >Company Name</label>
+                  <input type="text" className="form-control" id="companyNameInput" />
+                </div>
+                <div className="mb-2 mt-2">
+                  <label htmlFor="jobNumberInput" >Job Number</label>
+                  <input type="text" className="form-control" id="jobNumberInput" />
+                </div>
+                <div className="mb-2 mt-2">
+                  <label htmlFor="paperDetailsInput">Paper Details</label>
+                  <input type="text" className="form-control" id="paperDetailsInput" />
+                </div>
+                <div className="d-flex mb-2 mt-2">
+                  <div className="col">
+                    <label htmlFor="shippingStatusSelect">Shipping Status</label>
+                    <select name="" id="shippingStatusSelect" className="form-select fw-light">
+                      <option value="1">Shipped</option>
+                      <option value="2">Not Shipped</option>
+                    </select>
+                  </div>
+                  <div className="col">
+                    <label htmlFor="paymentStatusSelect">Payment Status</label>
+                    <select name="" id="paymentStatusSelect" className="form-select fw-light">
+                      <option value="1">WOP</option>
+                      <option value="2">Paid</option>
+                    </select>
+                  </div>
+                  <div className="col">
+                    <label htmlFor="shippingStatusSelect">Other Status</label>
+                    <select name="" id="shippingStatusSelect" className="form-select fw-light">
+                      <option value="1">WOO</option>
+                      <option value="2">WOA</option>
+                      <option value="3">ZNS</option>
+                      <option value="4">WOOA</option>
+                      <option value="5">NR</option>
+                      <option value="6">WOZ</option>
+                    </select>
+                  </div>
+                </div>
+              </form>
             </div>
             <div className="modal-footer">
               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
