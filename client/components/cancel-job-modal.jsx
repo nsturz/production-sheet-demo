17e8b95f424c901d2e9ba6f51926adabac2b4d1f
Nicolas Sturz <nicksturz92@gmail.com>
2023-05-09 23:25:21 +0000

    saving

--- client/components/cancel-job-modal.jsx
+++ client/components/cancel-job-modal.jsx
@@ -9,7 +9,7 @@ export default function CancelJobModal() {
         <i className="fa-solid fa-trash m-1 edit-icon" />
       </button>
       <div className="modal fade" id="cancelJobModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog modal-lg">
+        <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title text-center">Cancel Job</h5>
@@ -17,6 +17,10 @@ export default function CancelJobModal() {
             </div>
             <div className="modal-body">
               <h1> hello :3</h1>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+              </div>
             </div>
           </div>
         </div>
