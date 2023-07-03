0ccf7c5294e48ccce2ea3fdcd3cf4ff50ae7686c
Nicolas Sturz <nicksturz92@gmail.com>
2023-07-03 19:57:29 +0000

    working on styling for login page

--- client/pages/login-page.jsx
+++ client/pages/login-page.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
-export default function LoginPage(props) {
+export default function LoginPage() {
   return (
-    <h1>LOGIN PAGE</h1>
+    <div className="container">
+      <div className="d-flex justify-content-center pt-5">
+        <div className="col-8 login-wrapper rounded box-shadow mt-5 pb-5">
+          <div className="d-flex justify-content-center mt-3 mb-3">
+            <img src="/images/colorad.png" alt="" />
+          </div>
+          <div className="d-flex justify-content-center mt-3 mb-3">
+            <h2 className="fw-bold">Production Sheet</h2>
+          </div>
+          <form action="" className="d-flex justify-content-center mb-3 mt-3">
+            <div className="col-lg-10">
+              <div>
+                <input type="email" className="form-control mb-3 mt-3" placeholder="Email"/>
+                <input type="password" className="form-control mb-3 mt-3" placeholder="Password"/>
+              </div>
+              <div className="d-flex justify-content-center">
+                <button type="submit" className="btn login-btn">Submit</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
