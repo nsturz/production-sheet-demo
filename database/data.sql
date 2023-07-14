f173a9501857676cd67ca006e5722d03930eeb64
Nicolas Sturz <nicksturz92@gmail.com>
2023-07-14 21:21:40 +0000

    added users table to db

--- database/data.sql
+++ database/data.sql
@@ -1,3 +1,6 @@
+insert into "users" ("username", "hashedPassword")
+values ('anonymous', '$argon2i$v=19$m=4096,t=3,p=1$h7icQD/xZr8akZsX+hNA0A$h68atJWyjvunAwNOpSpMfg9sPvoMQ6dKwoh0dJhurWA');
+
 insert into "years" ("year")
 values      ('2023');
 
